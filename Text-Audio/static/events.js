bd60bdc9bfa97560657a87a237f8343a7a31b88c
kotarohith1729 <rohithkota1729@gmail.com>
2024-12-05 11:57:17 +0530

    Add files via upload

---
+++ Text-Audio/static/events.js
@@ -0,0 +1,14 @@
+let inputfield=document.getElementById('inp')
+let serachbutton =document.getElementById('ser')
+
+serachbutton.addEventListener('mouseenter', function() {
+    serachbutton.classList.add('no-hover');
+  });
+
+  serachbutton.addEventListener('click', function() {
+    inputField.classList.add('no-hover');
+  });
+
+  serachbutton.addEventListener('mouseleave', function() {
+    inputField.classList.add('no-hover');
+  });
